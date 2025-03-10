d20fdbce258fdc91e069ced6a8b3f32923c4751b
carlosmiguel222 <damilescarlosmiguel5@gmail.com>
2025-03-10 12:26:46 +0800

    Fixed gulp build error with del.deleteAsync

--- gulp/clean.gulp.js
+++ gulp/clean.gulp.js
@@ -1,18 +1,17 @@
-const gulp = require('gulp');
-const del = require('del');
-
-const { paths, baseDir, version } = require('./utils.js');
+const gulp = require("gulp"); // Keep CommonJS for gulp
+const { deleteAsync } = require("del"); // Correct way to import deleteAsync
+const { paths, baseDir, version } = require("./utils.js");
 
 /*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 |  Clean
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
-gulp.task('clean', () =>
-  del.deleteAsync([
+gulp.task("clean", () =>
+  deleteAsync([
     `${baseDir}/${paths.style.dest}/**/*.*`,
     `${baseDir}/${paths.script.dest}/**/*.*`,
     `${baseDir}/**/*.html`,
   ])
 );
 
-gulp.task('clean:build', () => del.deleteAsync(paths.dir.prod));
-gulp.task('clean:live', () => del.deleteAsync(`live/${version}`));
+gulp.task("clean:build", () => deleteAsync(paths.dir.prod));
+gulp.task("clean:live", () => deleteAsync(`live/${version}`));
